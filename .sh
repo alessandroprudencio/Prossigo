4a986116731f84523f82f295fd60edcd204c9d8c
alessandroprudencio <fretefiz-at-777468235334>
2020-02-18 22:23:50 -0400

    Criando script para dar o start no projeto

---
+++ .sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+
+PHPVersion=$(php -r 'echo PHP_MAJOR_VERSION;')
+InstalledComposer=$(Composer --version|grep --only-matching --perl-regexp "()\d+\.\\d+\.\\d+");
+InstalledLaravel=$(laravel -v|grep --only-matching --perl-regexp "()\d+\.\\d+\.\\d+");
+
+RecommendedComposer='1.9.3'
+RecommendedLaravel='3.0.1'
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+
+
+if [ "$PHPVersion" -le "6" ]; then
+    echo -e "${RED} Por favor atualize seu php e tente novamente!"
+    exit
+fi
+
+if [ ! which -a "composer" ]; then
+    echo -e " ${RED} Por favor instale o composer e tente novamente !"
+    exit
+fi
+
+if [ "yes" = "$(echo | awk "($InstalledComposer < $RecommendedComposer) { print \"yes\"; }")" ]; then 
+    echo -e "${RED} Por favor atualize seu composer e tente novamente!"
+    exit
+fi
+
+
+if [ ! which -a "laravel" ]; then
+    echo -e "${RED} Por favor instale o laravel e tente novamente !"
+    exit
+fi
+
+
+if [ "yes" = "$(echo | awk "($InstalledLaravel < $RecommendedLaravel) { print \"yes\"; }")" ]; then 
+    echo -e "${RED} Por favor atualize seu Laravel e tente novamente !"
+    exit
+fi
+
+
+echo Ola, vamos configurar esta aplicação, vamos começar pelo banco de dados...
+
+echo 'DB_DATABASE ( nome do banco )' ?
+read DbDatabase
+
+echo DB_USERNAME '( usuário adm do banco )' ?
+read DbUsername
+
+echo DB_PASSWORD '( senha adm do banco )' ?
+read DbPassword
+
+echo MAIL_DRIVER '( ex: smtp )' ?
+read MailDriver
+
+echo MAIL_HOST '( ex: smtp.gmail.com )' ?
+read MailHost
+
+echo MAIL_PORT '( ex: 587 )' ?
+read MailPort
+
+echo MAIL_USERNAME '( ex: teste@gmail.com )' ?
+read MailUsername
+
+echo MAIL_ENCRYPTION '( gmail use tls )' ?
+read MailEncryption
+
+echo MAIL_PASSWORD '( ex: seua_senha@2 )' ?
+read MailUsername
+
+echo MAIL_RECEIVES_MESSAGE '(  Após do prenchimento do formulário para qual email sera enviados os dados submetidos ? )'
+read MailReceivesMessage
+
+
+APP_NAME="Alessadro - Teste Prossigo"
+APP_ENV=local
+APP_KEY=
+APP_DEBUG=true
+APP_URL=http://localhost
+
+LOG_CHANNEL=stack
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=$DbDatabase
+DB_USERNAME=$DbUsername
+DB_PASSWORD=$DbPassword
+
+BROADCAST_DRIVER=log
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
+SESSION_DRIVER=file
+SESSION_LIFETIME=120
+
+MAIL_DRIVER=$MailDriver
+MAIL_HOST=$MailHost
+MAIL_PORT=$MailPort
+MAIL_USERNAME=$MailUsername
+MAIL_PASSWORD=$MailPassword
+MAIL_ENCRYPTION=$MailEncryption
+MAIL_RECEIVES_MESSAGE=$MailReceivesMessage
+MAIL_FROM_NAME="${APP_NAME}"
+" > teste.env
+
+
+
+# php artisan migrate
+
+
+echo  iniciando testes...
+
+test= vendor/bin/phpunit
+
+i=$(echo $?)
+if [[ $i != 0 ]]; then
+    echo -e " ${RED} Erro, os testes falharam."
+    exit
+fi
+
+
+
+php artisan serve
+
+
--- README.md
+++ README.md
@@ -10,9 +10,18 @@ Essas instruções fornecerão uma cópia do projeto em execução na sua máqui
 
 ```
 php": ^7.2
-laravel/framework: ^6.2
+laravel: ^6.2
 composer: ^1.9.3
 ```
+
+### Instalação Automatizada
+
+Dentro do projeto existe um arquivo .sh, abra a raiz execute o comando :
+```
+\.sh
+```
+Ao ser executado ira  verificar o php, laravel e composer instalado e sua maquina, em seguida solicitar para configurar o projeto para ser executado ** cuidado com os caracteres especiais ** pois esse arquivo sera usado para que tudo funcione corretamente, mas claro ocorra algum problema voce mesmo poder editar seu arquivo .env .Se tudo ocorrer bem, sera criado um arquivo ** .env **  no diretorio do projeto...Agora o script ira executar a criação das tabelas no banco de dados...Logo em seguida vem os testes, caso ocorra falha o serviço é interrompido, se não houver ele continuara e será iniciado o servidor para uso...  **
+
 
 ### Instalação
 
@@ -23,7 +32,7 @@ https://github.com/alessandroprudencio/Teste-Prossigo.git
 ```
 
 
-Para instalar as dependências , execute o comando na raiz do projeto
+Para instalar as dependências , execute o comando na raiz do projeto : 
 
 ```
 composer install
@@ -35,20 +44,20 @@ Copie o arquivo env de exemplo e faça as alterações necessárias na configura
 cp .env.example .env
 ```
 
-Gere uma nova chave de aplicativo
+Gere uma nova chave de aplicativo :
 
 ```
 php artisan key:generate
 ```
 
 
-Execute as migrações do banco de dados **( defina a conexão com o banco de dados em .env antes de migrar )**
+Execute as migrações do banco de dados **( defina a conexão com o banco de dados em .env antes de migrar )** :
 
 ```
 php artisan migrate
 ``` 
 
-Iniciando  servidor
+Iniciando  servidor :
 
 ```
 php artisan serve ou php artisan serve --port=8080
@@ -59,14 +68,14 @@ Agora você pode acessar o servidor em http://localhost:8000
 
 ## Executando os testes
 
-Para executar os testes basta abrir a raiz do projeto e executar o comando
+Para executar os testes basta abrir a raiz do projeto e executar o comando :
 ```
 vendor/bin/phpunit
 ```
 
 ## Semear banco de dados
 
-Execute o comando para semear o banco de dados
+Execute o comando para semear o banco de dados :
 
 ```
 php artisan db:seed
